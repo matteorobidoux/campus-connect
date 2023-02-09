ab3ec56061fa6fee576713fe4e490e2475505922
Marian <marian.hristov@dawsoncollege.qc.ca>
2023-02-09 18:18:34 -0500

    Add styling for smaller width pages

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import "./NavBar.css";
 
 function NavBar() {
-    return (
-      <div className="navbar">
-        <nav>
-            <h1 id="logo"> Campus Connect</h1>
-            <img id="profile" src="" alt="profile"></img>
-        </nav>
-      </div>
-    );
-  }
-  
-  export default NavBar;
+  return (
+    <div className="navbar">
+      <nav>
+        <h1 id="logo">Campus Connect</h1>
+        <img id="profile" src="" alt="profile"></img>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
