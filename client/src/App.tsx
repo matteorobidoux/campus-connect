6707a8f0fa2cb7214d50580b40dfd0cadb894bf0
Marian <marian.hristov@dawsoncollege.qc.ca>
2023-02-09 14:02:04 -0500

    Add components folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
+import Main from './Components/Main';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Main />
   );
 }
 
