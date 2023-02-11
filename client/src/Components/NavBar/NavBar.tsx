c8852f6d2597d2b30f330c08aeeabdc21124d9e9
elidjay <elidjay.ross@gmail.com>
2023-02-11 17:39:54 -0500

    added profile icon

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,11 +1,13 @@
+import { profile } from "console";
 import "./NavBar.css";
+import profileImg from "./profile.png";
 
 function NavBar() {
   return (
     <div className="navbar">
       <nav>
         <h1 id="logo">Campus Connect</h1>
-        <img id="profile" src="" alt="profile"></img>
+        <img id="profile" src={profileImg} alt="profile"></img>
       </nav>
     </div>
   );
