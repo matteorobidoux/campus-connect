001eb00f2a6535b99ddf4bab06cedef4d8c4acd7
Octavio <gancoc1@hotmail.com>
2023-02-13 08:40:03 -0500

    Renamed a File

---
+++ server/db/objects/courses.ts
@@ -0,0 +1,12 @@
+class Courses {
+  name: String;
+  section: Int32Array;
+  desc: String;
+
+  constructor(theName :String, theSection: Int32Array, theDesc: String){
+    this.name=theName;
+    this.section= theSection;
+    this.desc= theDesc;
+  }
+
+}
