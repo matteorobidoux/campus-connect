a8a795123062888ca344feaf2acecee9849f48ea
Octavio <gancoc1@hotmail.com>
2023-02-13 08:16:17 -0500

    Added courses and event Objects

---
+++ server/db/objects/event.ts
@@ -0,0 +1,12 @@
+class Events {
+  name: String;
+  deadline: Date;
+  desc: String;
+
+  constructor(theName :String, theDealine: Date, theDesc: String){
+    this.name=theName;
+    this.deadline= theDealine;
+    this.desc= theDesc;
+  }
+
+}
