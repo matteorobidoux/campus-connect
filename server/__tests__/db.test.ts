d9dffb6301f06709100cfec2d054808cf997146d
OCTAVIO GANCHOZO <gancoc1@hotmail.com>
2023-02-15 12:53:00 -0500

    Set up for micking Db

---
+++ server/__tests__/db.test.ts
@@ -0,0 +1,21 @@
+const mockingoose = require('mockingoose');
+import User from "../db/models/user-schema";
+
+
+describe('test mongoose User model', () => {
+  it('should return the doc with findById', () => {
+    const _doc = {
+      _id: '507f191e810c19729de860ea',
+      name: 'name',
+      email: 'name@email.com',
+    };
+
+    mockingoose(User).toReturn(_doc, 'findOne');
+
+    return User.findById({ _id: '507f191e810c19729de860ea' }).then(doc => {
+      expect(JSON.parse(JSON.stringify(doc))).toMatchObject(_doc);
+    });
+  });
+
+
+});
