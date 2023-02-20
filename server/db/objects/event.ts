eaf1bb7b37b52e67ccb750abadf41565f4e73417
OCTAVIO GANCHOZO <gancoc1@hotmail.com>
2023-02-20 12:04:17 -0500

    Formated Db file

--- server/db/objects/event.ts
+++ server/db/objects/event.ts
@@ -10,13 +10,4 @@ desc: string;
 associatedSect: AssociatedSection;
 }
 
-const eve:Events = {
-  id:"a",
-  title:"as",
-  deadline: new Date(),
-  desc:"as",
-  associatedSect:{name:"a"}
-}
-
-
 export default Events;
