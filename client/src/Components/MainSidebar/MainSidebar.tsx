503e196e45e54404c59ad17b3cb6d07c986c0df9
Marian <marian.hristov@dawsoncollege.qc.ca>
2023-02-27 19:22:01 -0500

    Fix horizontal wrapping on different widths

--- client/src/Components/MainSidebar/MainSidebar.tsx
+++ client/src/Components/MainSidebar/MainSidebar.tsx
@@ -47,6 +47,42 @@ export default function MainSidebar() {
             }
           ]
         },
+        {
+          title: "Organic Chemistry I",
+          number: "202-BZF-05",
+          sections: [
+            {
+              title: "Organic Chemistry I",
+              number: "00002",
+              teacher: "Carmen Leung",
+              schedule: "Tuesday8:30 AM - 10:00 AM3F.7ClassWednesday10:00 AM - 1:00 PM6A.13LabFriday8:30 AM - 10:00 AM3F.7Class"
+            }
+          ]
+        },
+        {
+          title: "Mise à niveau pour Français, langue seconde de la 5e secondaire",
+          number: "602-008-RE",
+          sections: [
+            {
+              title: "Mise à niveau pour Français, langue seconde de la 5e secondaire",
+              number: "00031",
+              teacher: "Francesca Roy",
+              schedule: "Tuesday10:00 AM - 12:00 PM4E.18ClassFriday10:00 AM - 12:00 PM4E.18Class"
+            }
+          ]
+        },
+        {
+          title: "Linear Algebra",
+          number: "201-105-DW",
+          sections: [
+            {
+              title: "Linear Algebra",
+              number: "00005",
+              teacher: "Gilbert Honnouvo",
+              schedule: "Tuesday2:00 PM - 4:00 PM4D.2LabWednesday1:00 PM - 2:30 PM4H.19ClassFriday1:00 PM - 2:30 PM4H.19Class"
+            }
+          ]
+        }
       ]
       let coloredCourses = courses as ColoredCourse[]
       try {
@@ -77,10 +113,6 @@ export default function MainSidebar() {
   return (
     <>
       <div className={styles["main-sidebar-container"]}>
-        <div className={[styles["sidebar-section"], styles["links"]].join(" ")}>
-          <h2>Bookstore</h2>
-          <h2>Calendar</h2>
-        </div>
         <div className={[styles["sidebar-section"], styles["classes"]].join(" ")}>
           <CourseQuickViewContainer courses={data} />
         </div>
