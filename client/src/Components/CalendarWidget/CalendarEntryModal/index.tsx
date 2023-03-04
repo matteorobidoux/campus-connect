ca0a89a2453a0d380ab81f4b71688cdec047ddb4
Matteo Robidoux <SDJ00008@gmail.com>
2023-03-04 17:23:46 -0500

    Updated PostEvent with Login and user functionality

--- client/src/Components/CalendarWidget/CalendarEntryModal/index.tsx
+++ client/src/Components/CalendarWidget/CalendarEntryModal/index.tsx
@@ -1,44 +1,24 @@
 import { Field, Form, Formik } from "formik";
 import { toast } from "react-toastify";
-<<<<<<< HEAD:client/src/Components/CalendarEntryModal/index.tsx
-import { useGetAllSections } from "../../custom-query-hooks";
 import styles from "./CalendarEntryModal.module.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useMutation } from "react-query";
-import { AddEventBody } from "../../../../types/Queries/AddEvent";
+import {AddEventBody} from "../../../../../types//Queries//AddEvent"
 import axios from "axios";
-=======
-import CalendarEvent from "../../../../../types/Calendar";
-import styles from "./CalendarEntryModal.module.scss"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useMutation } from "react-query";
 import { useSections, useUser } from "../../../custom-query-hooks";
->>>>>>> 3eacaab68efa39b86d4972754427482ff6b7d230:client/src/Components/CalendarWidget/CalendarEntryModal/index.tsx
 
 export interface CalendarEntryModalProps {
   onClose: () => void;
 }
 
-<<<<<<< HEAD:client/src/Components/CalendarEntryModal/index.tsx
 export default function CalendarEntryModal({onClose}: CalendarEntryModalProps) {  
   
-  const sections = useGetAllSections({userClassSections: [
-    {courseNumber: "tempCourseNumber", sectionNumber: "tempSectionNumber"},
-  ]});
+  const user = useUser();
+  const sectionsQuery = useSections({userClassSections: user.sections})
   
   const mutation = useMutation(async (arg: Omit<AddEventBody, 'id'>) => {
     axios.post('/api/addEvent', arg)
   });
-=======
-export default function CalendarEntryModal({onClose}: CalendarEntryModalProps) {
-  const user = useUser();
-  const sectionsQuery = useSections({userClassSections: user.sections});
-
-  const mutation = useMutation(async (arg: Omit<CalendarEvent, 'id'>) => {
-    console.log(arg);
-    await new Promise(r => setTimeout(r, 2000));
-  })
->>>>>>> 3eacaab68efa39b86d4972754427482ff6b7d230:client/src/Components/CalendarWidget/CalendarEntryModal/index.tsx
 
 
   const initialValues = {
@@ -55,7 +35,7 @@ export default function CalendarEntryModal({onClose}: CalendarEntryModalProps) {
         initialValues={initialValues}
         onSubmit={async (values) => {
           toast.loading("Adding event...", {toastId: 'addingEvent'});
-          const course = sections!.data!.response!.find(s => s.courseTitle === values.courseTitle)
+          const course = sectionsQuery!.data!.find(s => s.courseTitle === values.courseTitle)
           await mutation.mutateAsync({
             ...values,
             section: {
@@ -63,7 +43,7 @@ export default function CalendarEntryModal({onClose}: CalendarEntryModalProps) {
               sectionNumber: course!.number
             },
             event: {
-              ownerId: "tempOwnerID",
+              ownerId: user._id,
               date: values.date,
               title: values.courseTitle,
               desc: values.desc
@@ -110,11 +90,7 @@ export default function CalendarEntryModal({onClose}: CalendarEntryModalProps) {
               <label htmlFor="courseTitle"> Class </label>
               <Field as="select" id="courseTitle" name="courseTitle" value={'DEFAULT'}>
                 <option value="DEFAULT" disabled>Pick a class</option>
-<<<<<<< HEAD:client/src/Components/CalendarEntryModal/index.tsx
-                {sections.data?.response!.map(s => <option key={s.courseTitle}> {s.courseTitle} </option>)}
-=======
                 {sectionsQuery.data?.map(s => <option key={s.courseTitle}> {s.courseTitle} </option>)}
->>>>>>> 3eacaab68efa39b86d4972754427482ff6b7d230:client/src/Components/CalendarWidget/CalendarEntryModal/index.tsx
               </Field>
               <p> {errors.courseTitle ? errors.courseTitle : null} </p>
             </div>
