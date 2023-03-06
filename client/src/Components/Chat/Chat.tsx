b9b479802c6a678b8604f1fa878bfb29256901c9
elidjay <elidjay.ross@gmail.com>
2023-03-05 23:15:09 -0500

    added chat frontend

--- client/src/Components/Chat/Chat.tsx
+++ client/src/Components/Chat/Chat.tsx
@@ -4,13 +4,180 @@ type ChatProps = {
   selectedChat : String
 
 }
+//TODO: Change main bar depending on component selected
+//TODO: Change message input/button/route depending on groupchat selected
+//TODO: Make message into components
 
 export default function Chat(props: ChatProps) {
   return (
-    <div>
-      <div className={styles["chat-header"]}>
-        <h1>{props.selectedChat}</h1>
-      </div>
+    <div className={styles["main-chat"]}>
+      <section className={styles["msger"]}>
+        <header className={styles["msger-header"]}>
+          <div className={styles["msger-header-title"]}>
+            <i className={styles["fas fa-comment-alt"]}></i> 
+            {props.selectedChat}
+          </div>
+          <div className={styles["msger-header-options"]}>
+            <span><i className={styles["fas fa-cog"]}></i></span>
+          </div>
+        </header>
+
+        <main className={styles["msger-chat"]}>
+          <div className={styles["left-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>GANG</div>
+                <div className={styles["msg-info-time"]}>12:45</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                Chat chahtahhc hchhchchchchatttttt
+              </div>
+            </div>
+          </div>
+
+          <div className={styles["right-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>Jean Miche</div>
+                <div className={styles["msg-info-time"]}>12:46</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                CHAT CHCAHHCSHSHCHSsssss
+              </div>
+            </div>
+          </div>
+          <div className={styles["left-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>GANG</div>
+                <div className={styles["msg-info-time"]}>12:45</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                Chat chahtahhc hchhchchchchatttttt
+              </div>
+            </div>
+          </div>
+
+          <div className={styles["right-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>Jean Miche</div>
+                <div className={styles["msg-info-time"]}>12:46</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                CHAT CHCAHHCSHSHCHSsssss
+              </div>
+            </div>
+          </div>
+          <div className={styles["left-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>GANG</div>
+                <div className={styles["msg-info-time"]}>12:45</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                Chat chahtahhc hchhchchchchatttttt
+              </div>
+            </div>
+          </div>
+
+          <div className={styles["right-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>Jean Miche</div>
+                <div className={styles["msg-info-time"]}>12:46</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                CHAT CHCAHHCSHSHCHSsssss
+              </div>
+            </div>
+          </div>
+          <div className={styles["left-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>GANG</div>
+                <div className={styles["msg-info-time"]}>12:45</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                Chat chahtahhc hchhchchchchatttttt
+              </div>
+            </div>
+          </div>
+
+          <div className={styles["right-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>Jean Miche</div>
+                <div className={styles["msg-info-time"]}>12:46</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                CHAT CHCAHHCSHSHCHSsssss
+              </div>
+            </div>
+          </div>
+          <div className={styles["left-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>GANG</div>
+                <div className={styles["msg-info-time"]}>12:45</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                Chat chahtahhc hchhchchchchatttttt
+              </div>
+            </div>
+          </div>
+
+          <div className={styles["right-msg"]}>
+            <div className={styles["msg-img"]}></div>
+
+            <div className={styles["msg-bubble"]}>
+              <div className={styles["msg-info"]}>
+                <div className={styles["msg-info-name"]}>Jean Miche</div>
+                <div className={styles["msg-info-time"]}>12:46</div>
+              </div>
+
+              <div className={styles["msg-text"]}>
+                CHAT CHCAHHCSHSHCHSsssss
+              </div>
+            </div>
+          </div>
+        </main>
+
+        <form className={styles["msger-inputarea"]}>
+          <input type="text" className={styles["msger-input"]} placeholder="Enter your message..."></input>
+          <button type="submit" className={styles["msger-send-btn"]}>Send</button>
+        </form>
+</section>
+
+
     </div>
+
   )
 }
