efe49823dd31166e028d21968cc8f7cb46e9aaa9
elidjay <elidjay.ross@gmail.com>
2023-03-08 15:24:17 -0500

    log fix again

--- server/src/oauth/gauth-endpoint.ts
+++ server/src/oauth/gauth-endpoint.ts
@@ -23,7 +23,9 @@ export default async function GAuth(req: Request, res: Response<GAuthResponse>) 
 
     if (!gid || !email || !token.tokens.access_token || !token.tokens.refresh_token) {
       console.log(resp)
-      res.status(400).json({error: "" + resp});
+      console.log(token.tokens.access_token)
+      console.log(token.tokens.refresh_token)
+      res.status(400).json({error: "Couldn't find one of the required infos from google."});
       //res.status(400).json({error: "Couldn't find one of the required infos from google."});
       return;
     }
