089e1f276f5b6f5b285bf36e8770e655a6dc9aad
Octavio <gancoc1@hotmail.com>
2023-04-03 01:59:28 -0400

    Refactor with real Db test

--- server/src/__tests__/api.test.ts
+++ server/src/__tests__/api.test.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { app } from "../api-jest/app";
 import { UserClassSection } from "../../../types/UserClassSection";
 import { GetAllSectionsRequest } from "../../../types/Queries/GetAllCourses";
-jest.mock("../db/db.ts");
+jest.mock("../db/db");
 jest.mock("../oauth/gauth-endpoint")
 jest.mock("../oauth/index")
 afterAll(async () => {
@@ -21,13 +21,6 @@ describe("test Api Login", () => {
   it("should return 400 with no data", async () => {
     const res = await request(app).get("/api/login");
     expect(res.status).toBe(404);
-  });
-});
-
-describe("test api getAllStrippedCourses", () => {
-  it("should return 400", async () => {
-    const res = await request(app).get("/api/getAllStrippedCourses");
-    expect(res.status).toBe(400)
   });
 });
 
@@ -66,175 +59,6 @@ describe("test api getLatestMessages", () => {
 });
 
 
-describe("test api getMostRecentMessage", () => {
-  it("should return 200 res body Defined", async () => {
-    const room = {
-       courseNumber: "574-453-DW",
-      sectionNumber: "00001"
-     
-    };
-    const res = await request(app).get("/api/getMostRecentMessage").send(room);
-    expect(res.body).toBeDefined();
-  });
-});
 
-describe("test api getLatestMessages", () => {
-  it("should return 400 missig req param", async () => {
-    const room = {
-      room: {courseNumber: "574-453-DW",
-      sectionNumber: "00001"},
-    };
-    const res = await request(app).get("/api/getLatestMessages").send(room);
-    expect(res.status).toBe(400);
- 
-  });
-});
 
-describe("test api getAllSections", () => {
-  it("should return not undefined", async () => {
-    const room : UserClassSection[]=[ {
-       courseNumber: "574-453-DW",
-      sectionNumber: "00001",
-      
-    }];
-    const abe:GetAllSectionsRequest={userClassSections:room};
-    
-    const res = await request(app).get("/api/getAllSections").send(abe.userClassSections);
-    expect(res.status).toBe(400);
-  });
-});
 
-describe("test api getAllSections", () => {
-  it("should return 400 error", async () => {
-    const room = {
-      courseNumber: "574-453-DW",
-    };
-    const res = await request(app).get("/api/getAllSections").send("room");
-    expect(res.status).toBe(400);
-  });
-});
-
-describe("test api removeEvent", () => {
-  it("should return status 200", async () => {
-    const res = await request(app).post("/api/removeEvent").send({
-      eventId: "640ac0b131483eb8b8a1cb81",
-      courseNumber: "574-453-DW",
-      courseSection: "00001",
-    });
-    expect(res.statusCode).toBe(200);
-  });
-});
-
-describe("test api removeEvent", () => {
-  it("should return status 400 missing a parameter", async () => {
-    const res = await request(app).post("/api/removeEvent").send({
-      eventId: "640ac0b131483eb8b8a1cb81",
-      courseNumber: "574-453-DW",
-    });
-    expect(res.statusCode).toBe(400);
-  });
-});
-
-describe("test api appLogin", () => {
-  it("should return status 200", async () => {
-    const res = await request(app).post("/api/login").send({
-      name: "tesNameJesus",
-      password: "574-453-DW",
-    });
-    expect(res.statusCode).toBe(200);
-  });
-});
-
-describe("test api appLogin", () => {
-  it("should return status 400", async () => {
-    const res = await request(app).post("/api/login").send({
-      name: "tesNameJesus",
-    });
-    expect(res.statusCode).toBe(400);
-  });
-});
-
-describe("test api addEvent", () => {
-  it("should return 200", async () => {
-    const res = await request(app)
-      .post("/api/addEvent")
-      .send({
-        section: { courseNumber: "574-453-DW", sectionNumber: "00001" },
-        event: {
-          ownerId: "640ac0b131483eb8b8a1cb82",
-          date: Date,
-          title: "Today Carrots",
-          desc: "carrots and eyes",
-          courseTitle: "Story Board",
-        },
-      });
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toStrictEqual({ success: true });
-  });
-});
-
-describe("test api addEvent", () => {
-  it("should return 400 missing a parameter", async () => {
-    const res = await request(app)
-      .post("/api/addEvent")
-      .send({
-        event: {
-          ownerId: "640ac0b131483eb8b8a1cb82",
-          date: Date,
-          title: "Today Carrots",
-          desc: "carrots and eyes",
-          courseTitle: "Story Board",
-        },
-      });
-    expect(res.statusCode).toBe(400);
-  });
-});
-
-describe("test api addUser", () => {
-  it("should return 200", async () => {
-    const user = {
-      gid: "string",
-      email: "string",
-      name: "string",
-      picture: "string",
-      sections: [],
-      googleTokens: {
-        refresh_token: "string",
-        access_token: "string",
-      },
-    };
-    const res = await request(app).post("/api/addUser").send({
-      user,
-    });
-    expect(res.statusCode).toBe(200);
-  });
-});
-
-describe("test api addCompletedEvent", () => {
-  it("should return 200", async () => {
-    const res = await request(app)
-      .post("/api/addCompletedEvent")
-      .send({
-        userId: "string",
-        completedEvent: {
-          id: "string",
-          date: Date,
-        },
-      });
-    expect(res.statusCode).toBe(200);
-  });
-});
-
-describe("test api addCompletedEvent", () => {
-  it("should return 400 a parameter is missing", async () => {
-    const res = await request(app)
-      .post("/api/addCompletedEvent")
-      .send({
-        completedEvent: {
-          id: "string",
-          date: Date,
-        },
-      });
-    expect(res.statusCode).toBe(400);
-  });
-});
--- server/src/jest.config.js
+++ server/src/jest.config.js
@@ -2,5 +2,12 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  coveragePathIgnorePatterns:["./db/db.ts",  "./oauth"]
+  coveragePathIgnorePatterns:["./db/db.ts",  "./oauth"],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
 };
